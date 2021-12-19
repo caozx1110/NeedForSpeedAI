84adcdca5dd68d29808f6758fc3ade288d943e7d
mcl <2017179640@qq.com>
2021-12-19 13:50:15 +0800

    ming

--- App/Drive.py
+++ App/Drive.py
@@ -26,6 +26,12 @@ import KeyboardEmulation as k
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+class_encoding = OrderedDict([
+    ('unlabeled', (0, 0, 0)),
+    ('road', (255, 0, 0)),
+    ('car', (0, 255, 0)),
+])
+
 # label to key dict
 # Dict = {
 #     'W': 0,
@@ -48,7 +54,7 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 #     8: ''
 # }
 
-Dict = {'A': 0, 'D': 1, 'W': 2, 'WA': 3, 'WD': 4, 0: 'A', 1: 'D', 2: 'W', 3: 'WA', 4: 'WD'}
+Dict = {'A': 0, 'D': 1, 'N': 2, 'W': 3, 'WA': 4, 'WD': 5, 0: 'A', 1: 'D', 2: 'N', 3: 'W', 4: 'WA', 5: 'WD'}
 
 # class PredThread(Thread):
 #     """预测线程"""
@@ -90,7 +96,7 @@ class KeyThread(Thread):
     CodeDic = {
         'W': 0x11,
         'A': 0x1e,
-        'F': 0x1f,
+        'S': 0x1f,
         'D': 0x20
     }
 
@@ -98,23 +104,29 @@ class KeyThread(Thread):
         super(KeyThread, self).__init__()
         self.ScanCode = []
         self.DoRun = True
+        self.DoPause = False
 
     def run(self) -> None:
         """run"""
         while self.DoRun:
+            while self.DoPause:
+                time.sleep(0.1)
+
             if self.ScanCode:
                 for c in self.ScanCode:
-                    k.key_down(c)
-                    # print("press", c)
+                    k.key_press(c, 0.01)
+                    # k.key_down(c)
+                    # # print("press", c)
                     # time.sleep(0.0001)
+                # time.sleep(0.001)
 
     def ChangeKey(self, key):
         """
         :param key: the list of the pressing key, e.g. 'WA'
         :return: None
         """
-        for _k in self.ScanCode:
-            k.key_up(_k)
+        # for _k in self.ScanCode:
+        #     k.key_up(_k)
         self.ScanCode = []
         for _k in key:
             self.ScanCode.append(self.CodeDic[_k])
@@ -122,16 +134,23 @@ class KeyThread(Thread):
     def Stop(self):
         self.DoRun = False
 
+    def Pause(self):
+        self.DoPause = True
+
+    def Restart(self):
+        self.DoPause = False
+
 
 if __name__ == "__main__":
     print("loading model...")
     # 分割网络
     SegModel = ENet(num_classes=3).to(DEVICE)
-    checkpoint = torch.load('./save/nfs_enet', map_location=torch.device(DEVICE))
+    checkpoint = torch.load('./save/aug2/nfs_enet', map_location=torch.device(DEVICE))
     SegModel.load_state_dict(checkpoint['state_dict'])
     # 分类网络
-    ClassModel = torch.load("./save/drive.pth", map_location=torch.device(DEVICE))
+    ClassModel = torch.load("./save/max.pth", map_location=torch.device(DEVICE))
     print('model loaded!')
+    print(DEVICE)
     # 按键线程
     KThread = KeyThread()
 
@@ -152,11 +171,15 @@ if __name__ == "__main__":
     # press E to stop
     current = ''
     while True:
-        # st = time.time()
-        # img = pyautogui.screenshot(region=[0, 0, 640, 480])  # x,y,w,h
+        st = time.time()
+        img = pyautogui.screenshot(region=[0, 0, 640, 480])  # x,y,w,h
         # img.save('./temp.png')
         # Seg
+        # img = Image.open('./temp.png')
         img = alter_predict(SegModel, img, DEVICE)
+        # render = pre2render(img, class_encoding)
+        # plt.imshow(render)
+        # plt.show()
         # 归一化
         img = img / 2
         # class
@@ -174,38 +197,49 @@ if __name__ == "__main__":
             print('end')
             KThread.Stop()
             break
+        # pause
+        if is_pressed('p'):
+            print('pause')
+            KThread.Pause()
+            while True:
+                time.sleep(0.1)
+                # restart
+                if is_pressed('r'):
+                    KThread.Restart()
+                    break
+
         # 一个循环用时
         print("time", time.time() - st)
 
 
-import cv2
-import matplotlib.pyplot as plt
-SegModel = ENet(num_classes=3).to(DEVICE)
-checkpoint = torch.load('./save/nfs_enet', map_location=torch.device(DEVICE))
-SegModel.load_state_dict(checkpoint['state_dict'])
-# 分类网络
-
-color_encoding = OrderedDict([
-    ('unlabeled', (0, 0, 0)),
-    ('road', (128, 64, 128)),
-    ('car', (64, 0, 128)),
-])
-ClassModel = torch.load("./save/drive.pth", map_location=torch.device(DEVICE))
-root = '..\\data\\Collect\\data_raw\\czx1'
-img_list = os.listdir(root)
-i = 0
-i = i + 1
-plt.close()
-item = img_list[i]
-img = Image.open(os.path.join(root, item))
-lbl = alter_predict(SegModel, img, DEVICE)
-x = pre2render(lbl, color_encoding)
-plt.subplot(121)
-plt.imshow(img)
-plt.axis('off')
-plt.subplot(122)
-plt.imshow(x)
-plt.axis('off')
-pred = Dict[tensor_predict(ClassModel, lbl / 2, DEVICE).item()]
-print(pred)
+# import cv2
+# import matplotlib.pyplot as plt
+# SegModel = ENet(num_classes=3).to(DEVICE)
+# checkpoint = torch.load('./save/nfs_enet', map_location=torch.device(DEVICE))
+# SegModel.load_state_dict(checkpoint['state_dict'])
+# # 分类网络
+#
+# color_encoding = OrderedDict([
+#     ('unlabeled', (0, 0, 0)),
+#     ('road', (128, 64, 128)),
+#     ('car', (64, 0, 128)),
+# ])
+# ClassModel = torch.load("./save/drive.pth", map_location=torch.device(DEVICE))
+# root = '..\\data\\Collect\\data_raw\\czx1'
+# img_list = os.listdir(root)
+# i = 0
+# i = i + 1
+# plt.close()
+# item = img_list[i]
+# img = Image.open(os.path.join(root, item))
+# lbl = alter_predict(SegModel, img, DEVICE)
+# x = pre2render(lbl, color_encoding)
+# plt.subplot(121)
+# plt.imshow(img)
+# plt.axis('off')
+# plt.subplot(122)
+# plt.imshow(x)
+# plt.axis('off')
+# pred = Dict[tensor_predict(ClassModel, lbl / 2, DEVICE).item()]
+# print(pred)
 
